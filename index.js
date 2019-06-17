d3c57ce0bb6f3683a52a344d0b0ce49e32d2641c
David Strauch <dmaster18@gmail.com>
2019-06-17 14:35:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n){
+  var rankedListAmounts = document.querySelectorAll('.ranked-list');
+  for (var i = 0; i < rankedListAmounts.length; i++) {
+    rankedListAmounts[i].innerHTML = parseInt(rankedListAmounts[i].innerHTML) + n;
+  }
+  }
+  
+function deepestChild(){
+  return div
+}
 

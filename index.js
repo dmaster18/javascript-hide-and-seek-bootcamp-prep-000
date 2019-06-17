2f86cd52f5f0877205f0632d04c1b7eaebf5b01a
David Strauch <dmaster18@gmail.com>
2019-06-17 15:02:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n){
+  var rankedListAmounts = document.querySelectorAll('.ranked-list');
+  for (var i = 0; i < rankedListAmounts.length; i++) {
+    rankedListAmounts[i].innerHTML = parseInt(rankedListAmounts[i].innerHTML) + n;
+  }
+  }
+  
+function deepestChild() {
+  let gNode = document.querySelector('#grand-node')
+  let nextNode = gNode.children[0];
+  
+  while (nextNode) {
+    gNode = nextNode;
+    nextNode = gNode.children[0];
+  }
+  return (gNode);
+}
 
